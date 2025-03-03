56b0869f72e68e278f212e45a726b92ab3d391cf
bayan kassem <bayanka@post.bgu.ac.il>
2025-03-03 12:00:14 +0200

    added general nutritions to preserve our limited api calls

--- business_logic/api_manager.py
+++ business_logic/api_manager.py
@@ -11,10 +11,12 @@ class API_Manager:
             "q": food_name,
             "api_key": api_key
         }
-        search = GoogleSearch(params)
-        results = search.get_dict()
-        nutrition_information = results["knowledge_graph"]['list']
-        return nutrition_information
+        # search = GoogleSearch(params)
+        # results = search.get_dict()
+        # nutrition_information = results["knowledge_graph"]['list']
+
+        # return nutrition_information
+        return {'total_fat': ['17 g', '26%'], 'cholesterol': ['56 mg', '18%'], 'sodium': ['497 mg', '20%'], 'potassium': ['271 mg', '7%'], 'total_carbohydrate': ['29 g', '9%'], 'protein': ['20 g', '40%']}
 
 
 
