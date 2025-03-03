350948c7d63e61ac3979e5f4a3d0bbf56c5fadef
bayan kassem <bayanka@post.bgu.ac.il>
2025-03-03 17:35:27 +0200

    minor fixes to generate_report_by_date

--- fake_creator.py
+++ fake_creator.py
@@ -1,5 +1,8 @@
+from datetime import datetime
 from random import randint
 from faker import Faker
+
+import DAO.dao_controller
 
 
 def create_fake_data(amount: int) -> list:
@@ -11,12 +14,22 @@ def create_fake_data(amount: int) -> list:
             "potassium": f'{randint(1, 300)} mg',
             "total_carbohydrate": f'{randint(1, 100)} g',
             "protein": f'{randint(1, 100)} g',
-            "user_id": randint(1, 999999999),
+            "user_id": 7449332894,
             # date example: 2025-03-03T16:05:27.863+00:00
-            "date": f'{randint(2020, 2025)}-{randint(1, 12)}-{randint(1, 28)}'
+            "date": f'{randint(2025, 2025)}-{randint(1, 12)}-{randint(1, 28)}'
                     f'T{randint(0, 23)}:{randint(0, 59)}:{randint(0, 59)}'
                     f'.{randint(0, 999)}+00:00',
             "name": Faker().name()
         }
         for i in range(amount)
     ]
+
+
+dao_controller=DAO.dao_controller.DaoController()
+list_of_foods=create_fake_data(15)
+for food in list_of_foods:
+    # dao_controller.add_food_fake(food)
+    timestamp_format = "%Y-%m-%dT%H:%M:%S.%f+00:00"
+    datetime_obj = datetime.strptime(food['date'], timestamp_format)
+    food['date']=datetime_obj
+    dao_controller.add_food_fake(food)
