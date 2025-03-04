aa73df67578b906bfd3f6013332ce4e3b510d1a6
Or H. <moxer1@gmail.com>
2025-03-05 00:27:13 +0200

    Closed #41: * Implemented the gemini functions into the main code, including new menu section.
    * Motivation messages for now or only per user request.
    
    * Improvement advice for now or only for a selected day.
    
    * Added bot profile pictures, fixed the visibility of the "business_controller.py" code a little bit & added some more logs to the chart generation functions.
    
    (!) improvement advice didn't worked in the test, as the bot not performing the function at all - will be solved in a future issue.

--- business_logic/gemini_controller.py
+++ business_logic/gemini_controller.py
@@ -1,7 +1,10 @@
+import telebot
 from google import genai
 from google.genai import types
 
 import bot_secrets
+from DAO.dao_controller import DaoController as Dao
+# from datetime import datetime
 
 
 # ToDo: Add try-except in case of api-connection error
@@ -36,9 +39,42 @@ class GeminiController:
         )
         # response = self.chat.send_message("Give the user a motivational message to keep them on track with their diet.")
         # response = self.chat.send_message(types.Part.from_text("Give the user a motivational message to keep them on track with their diet."))
-        return response
+        return response.text
 
-    def improvement_advice(self, history:str):
+    def improvement_advice(self, user_id:int, date:telebot.types.Message):
+        # data = Dao().get_foods_by_user_and_date(user_id, datetime.now().date().strftime("%d.%m.%y"))
+        data = Dao().get_foods_by_user_and_date(user_id, date)
+
+        labels = ['calories', 'total_fat', 'cholesterol', 'sodium',
+                  'total_carbohydrate', 'potassium', 'protein', 'sugars']
+        details = {}
+        history = ''
+
+        for label in labels:
+            details[label] = .0
+
+        for food in data:
+            details['calories'] += float(food['calories'].split()[0])
+            details['total_fat'] += float(food['total_fat'].split()[0])
+            details['cholesterol'] += float(food['cholesterol'].split()[0])
+            details['sodium'] += float(food['sodium'].split()[0])
+            details['total_carbohydrate'] += float(food['total_carbohydrate'].split()[0])
+            details['potassium'] += float(food['potassium'].split()[0])
+            details['protein'] += float(food['protein'].split()[0])
+            details['sugars'] += float(food['sugars'].split()[0])
+
+        details['calories'] = str(details['calories'])
+        details['total_fat'] = str(details['total_fat'] + ' gr')
+        details['cholesterol'] = str(details['cholesterol'] + ' mg')
+        details['sodium'] = str(details['sodium'] + ' mg')
+        details['total_carbohydrate'] = str(details['total_carbohydrate'] + ' gr')
+        details['potassium'] = str(details['potassium'] + ' mg')
+        details['protein'] = str(details['protein'] + ' gr')
+        details['sugars'] = str(details['sugars'] + ' gr')
+
+        for key, value in details.items():
+            history += key + ': ' + value + '\n'
+
         response = self.client.models.generate_content(
             model="gemini-2.0-flash",
             config=types.GenerateContentConfig(
